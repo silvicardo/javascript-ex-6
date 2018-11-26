9f7a9dc91ecf1edb8aa8571d6c24c3ecf3bcf595
silvicardo <silvicardo@gmail.com>
2018-11-26 15:30:16 +0100

    domManipulation

--- domManipulation/main.js
+++ domManipulation/main.js
@@ -3,3 +3,10 @@ sesso con due prompt diversi.
 Sulla pagina html deve apparire “Ciao <nome>”, il
 colore del nome deve essere azzurro o rosa a
 seconda del sesso inserito*/
+
+var nomeCompletoUtente = prompt('Digita il tuo nome completo.');
+var sessoUtente = (prompt('Digita il tuo sesso(maschile o femminile).')).toLowerCase();
+var nomeCompletoHtml = document.getElementById('nomeCompleto');
+nomeCompletoHtml.innerHTML = "Ciao " + nomeCompletoUtente;
+
+nomeCompletoHtml.style.color = (sessoUtente == 'maschile') ? 'cyan' : 'pink';
